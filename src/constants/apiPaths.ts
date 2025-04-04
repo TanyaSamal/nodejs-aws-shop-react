a268419f953860bed6b21b4e37c2f089c951b6e3
TanyaSamal <solesytto@gmail.com>
2025-04-04 13:37:17 +0300

    Task8: integrate FE with BE lambda function

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
   product: "https://lakqnds8fg.execute-api.eu-north-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order:
+    "https://5i55hfe5lsdzkevstiwhohefdy0fhkno.lambda-url.eu-north-1.on.aws/api/profile/cart",
   import: "https://3p42f3tup8.execute-api.eu-north-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://5i55hfe5lsdzkevstiwhohefdy0fhkno.lambda-url.eu-north-1.on.aws/api",
 };
 
 export default API_PATHS;
